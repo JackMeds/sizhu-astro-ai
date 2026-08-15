5d49c773248633b0e1b4370031a3ae2112441d5d
JackMeds <38741783+JackMeds@users.noreply.github.com>
2026-08-15 08:31:15 +0800

    seo: load guide hub styles

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./styles/app.css";
 import "./styles/refresh.css";
+import "./styles/seo.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
