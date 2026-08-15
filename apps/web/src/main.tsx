8a3a7ea212d42c870b6abd2a6cf7010933f2e7a4
JackMeds <38741783+JackMeds@users.noreply.github.com>
2026-08-15 09:42:52 +0800

    web: load sponsor styling

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -7,6 +7,7 @@ import "./styles/seo.css";
 import "./styles/evidence.css";
 import "./styles/transit.css";
 import "./styles/handoff.css";
+import "./styles/sponsor.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
